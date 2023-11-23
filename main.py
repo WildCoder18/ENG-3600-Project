7163549481c76df290a6c42bd52856da0cec5ffc
Chris <dogbot460@gmail.com>
2023-11-22 22:55:01 -0500

    11/22 Changes

--- main.py
+++ main.py
@@ -1,4 +1,18 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, jsonify
+import random
+
+
+questions = [
+    {
+        "question": "What is the capital of France?",
+        "choices": ["Paris", "Berlin"]
+    },
+    {
+        "question": "Which planet is known as the Red Planet?",
+        "choices": ["Mars", "Venus"]
+    },
+    # Add more questions as needed
+]
 
 app = Flask(__name__)
 
@@ -10,6 +24,21 @@ def home():
 @app.route('/Prologue')
 def Prologue():
     return render_template('Prologue.html')
+
+@app.route('/Prologue_2')
+def Prologue_2():
+    return render_template('Prologue_2.html')
+
+@app.route('/Game_Section_1')
+def Game_Section_1():
+    return render_template('Game_Section_1.html')
+
+@app.route('/get_random_question', methods=['GET'])
+def get_random_question():
+    # Select a random question from the list
+    random_question = random.choice(questions)
+    return jsonify(random_question)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
